597a51bc7e5a95f3ef3443ae780f560fbdb17b76
jukaveka <juhokavekari@hotmail.com>
2026-05-07 16:24:51 +0300

    1 - Test that correct amount of days are returned

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -7,4 +7,9 @@ describe("Untestable 1: days until Christmas", () => {
     const testDate = new Date(2025, 4, 7, 3, 0, 0, 0);
     expect(testableDaysUntilChristmas(testDate)).to.be.a("number");
   });
+
+  test("returns correct amount of days", () => {
+    const testDate = new Date(2025, 12 - 1, 22, 3, 0, 0, 0);
+    expect(testableDaysUntilChristmas(testDate)).to.be.equal(3, "kolme yötä jouluun on...");
+  });
 });
